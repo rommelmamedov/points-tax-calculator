ac2dfcaf3c62d17cc91f91709dec6c3dcd732cad
rommelmamedov <rommelmamedov@gmail.com>
2023-02-07 18:04:14 +0400

    setup tests

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,5 @@
+/// <reference types="vitest" />
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
@@ -6,4 +8,13 @@ import eslintPlugin from '@nabla/vite-plugin-eslint';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), viteTsconfigPaths(), eslintPlugin()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: 'src/tests/_setup.ts',
+    coverage: {
+      reporter: ['text', 'html'],
+      exclude: ['node_modules/', 'src/tests/_setup.ts'],
+    },
+  },
 });
